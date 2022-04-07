b2df4abf80f97ec8c28780bd903a6b912dfff3d9
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-04-07 09:10:48 +0700

    test(user): add user test suites
    * add user test suites for create user

--- src/routes/test/user.route.test.js
+++ src/routes/test/user.route.test.js
@@ -1,17 +1,29 @@
 const request = require('supertest');
 
 const app = require('../../app');
-const UserModel = require('../../models/user.model')
+const pool = require('../../pool');
+const UserModel = require('../../models/user.model');
 
-it('should be create a user', () => {
-    const start = await UserModel.count();
-    expect(start).toEqual(0)
-    
-    await request(app())
-    .post('/users')
-    .send({ username: 'testuser', bio: 'test bio'})
-    .expect(200)
+beforeAll(() => {
+	pool.connect({
+		host: 'localhost',
+		port: 5432,
+		database: 'socialnetwork',
+		user: 'tamagossi',
+		password: '',
+	});
+});
 
-    const finish = await UserModel.count();
-    expect(finish).toEqual(1)
+afterAll(() => {
+	return pool.close();
+});
+
+it('should be create a user', async () => {
+	const start = await UserModel.count();
+	expect(start).toEqual(0);
+
+	await request(app()).post('/users').send({ username: 'testuser', bio: 'test bio' }).expect(200);
+
+	const finish = await UserModel.count();
+	expect(finish).toEqual(1);
 });
